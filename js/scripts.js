8354c969df7010a30e9ac9936eaf231a37d7b7b6
Henry Javangwe <henrysparks1@gmail.com>
2020-04-01 23:50:43 +0200

    Added more scripts to the shopping carts scripts. created a function that will count the number of products added to the cart and update the number on the basket icon. prior to that an array of objects with the courses was created and adding a click event listener to all the add-to-inquiry buttons, then, A function that will look at the local storage numbers and sat the number on the basket icon to be exactly as the number in the local storage- in order to keep the number from disappearing when the page is refreshed was created.

--- js/scripts.js
+++ js/scripts.js
@@ -58,48 +58,88 @@ window.onload = function() {
     
 };
 
-// CAREER SERVICES CAROUSEL.
-// ADDING AND REMOVING THE GRAYSCALE CLASS ON MOUSE HOVER.
-
-// const carouselItem = document.getElementById("grayscale");
-// item.addEventListener("mouseover", func, false);
-// item.addEventListener("mouseout", func1, false);
-
-// function func()
-// {  // not needed since item is already global, 
-//    // I am assuming this is here just because it's sample code?
-//    // var item = document.getElementById("button"); 
-//    item.setAttribute("style", "background-color:blue;")
-// }
-
-// function func1()
-// {  
-//    item.setAttribute("style", "background-color:green;")
-// }
-
-
-
-//COURSE OUTLINE SCRIPTS TO TOGGLE DIFFERENT TABS 
-
-
 // SHOPPING CART SCRIPTS
-console.log("running")
+
+console.log("running");
+
 const addToInquiry = document.querySelectorAll("#add-to-inquiry");
+
+let courses = [
+    {
+        name    : 'Become a Full Stack Web Developer ',
+        tag     : 'Full Stack Web Development',
+        price   : '26000',
+        inCart  : 0
+    },
+    {
+        name    : 'Become a Front End Web Developer ',
+        tag     : 'Front End Web Development',
+        price   : '9000',
+        inCart  : 0
+    },
+    {
+        name    : 'Become a Java Spring Boot Developer',
+        tag     : 'Java Spring Boot Development',
+        price   : '16000',
+        inCart  : 0
+    },
+];
+
+// adding a click event listener to all the add-to-inquiry buttons.
 
 for (let i =0; i<addToInquiry.length; i++){
     addToInquiry[i].addEventListener('click', ($event)=>{
         $event.preventDefault();
-        cartNumbers();
+        cartNumbers(courses[i]);
+        // totalCost(courses[i]);
     })
 };
-
-function cartNumbers (){
-    localStorage.setItem('cartNumbers', 1);
-    let productNumbers = localStorage.getItem('cartNumbers');
-    console.log(productNumbers);
-    productNumbers = parseInt(productNumbers);
-    console.log(typeof productNumbers);
+// A function that will look at the local storage numbers and sat the number on the basket icon to be exactly as the number in the local storage- in order to keep the number from disappearing when the page is refreshed
+function onloadCartNumbers (){
+    let courseNumbers = localStorage.getItem('cartNumbers');
     
-    
-    
+    if(courseNumbers){
+        document.querySelector('.basket-value').textContent = courseNumbers;
+    }
 };
+
+// A FUNCTION that will count the number of products added to the cart and update the number on the basket icon.
+
+function cartNumbers (course){
+    let courseNumbers = localStorage.getItem('cartNumbers');
+    courseNumbers = parseInt(courseNumbers);
+    if (courseNumbers) {
+        localStorage.setItem('cartNumbers', courseNumbers + 1);
+        document.querySelector('.basket-value').textContent = courseNumbers+ 1;
+    } else {
+        localStorage.setItem('cartNumbers',1);
+        document.querySelector('.basket-value').textContent = 1;
+    }
+    setItems(course);
+};
+
+
+function setItems (course){
+    let cartItems = localStorage.getItem('coursesInCart');
+    cartItems = JSON.parse(cartItems);
+    console.log('my cart items are', cartItems);
+    if (cartItems != null){
+        
+        if(cartItems[course.tag] == undefined){
+            cartItems = {
+                ...cartItems,
+                [course.tag]: course
+            }
+        }
+        cartItems[course.tag].inCart += 1; 
+    }else {
+        course.inCart =1;
+        cartItems ={
+            [course.tag]: course
+        }
+    } 
+    localStorage.setItem("coursesInCart", JSON.stringify(cartItems));
+};
+
+// calling the onload function so that it runs.
+onloadCartNumbers ();
